a203dca0681c2c7d682ff94848a167702a3eb7da
MOPER <oswaldomoyap@gmail.com>
2024-10-14 18:33:50 +0000

    refactor

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   description = "Helloyesod flake";
 
   inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     haskellNix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -11,51 +15,58 @@
                    , nixpkgs
                    , flake-utils
                    , haskellNix
+                   , flake-compat
                    }:
 
-  let
-    overlays =
-      [
-        haskellNix.overlay
-        ( final : prev: {
-          helloyesod = final.haskell-nix.stackProject {
+    let
+      overlays =
+        [ haskellNix.overlay
+          (final: prev: {
+            helloyesod = final.haskell-nix.stackProject {
+
               src = final.haskell-nix.cleanSourceHaskell {
                 src = ./.;
                 name = "helloyesod";
               };
+
               shell.buildInputs = with pkgs; [
                 stack
+                haskell.compiler.ghc98
+                haskellPackages.zlib_0_7_1_0
                 nixUnstable
                 # stylish-haskell
                 # haskell-language-server
               ];
               shell.additional = hsPkgs: with hsPkgs; [ Cabal ];
-          };
-          # helloyesod-wrapper = pkgs.writeShellApplicaction {
-          #   name = "helloyesod-wrapped";
-          #   runtimeinputs = [ self.packages.x86_64-linux.default ];
-          #   text = ''
-          #     cd /home/omoper/helloyesod
-          #     ${self.packages.x86_64-linux.default}/bin/helloyesod
-          #   '';
-          # };
-        })
-      ];
-    pkgs = import nixpkgs { system = "x86_64-linux";
-                            inherit overlays;
-                            inherit (haskellNix) config;
-                          };
-    flake = pkgs.helloyesod.flake {};
-  in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: flake // {
-      packages = flake.packages // {
-        default = flake.apps."helloyesod:exe:helloyesod";
-        helloyesod-wrapper = pkgs.helloyesod-wrapper;
-      };
-      apps = flake.apps // { default = flake.apps."helloyesod:exe:helloyesod"; };
+            };
+            # helloyesod-wrapper = pkgs.writeShellApplication {
+            #   name = "helloyesod-wrapped";
+            #   runtimeInputs = [ self.packages.x86_64-linux.default ];
+            #   text = ''
+            #    cd /home/omoper/helloyesod
+            #    ${self.packages.x86_64-linux.default}/bin/helloyesod
+            #   '';
+            # };
 
-      legacyPackages = pkgs;
-    });
-   # --- Flake Local Nix Configuration ----------------------------
+          })
+        ];
+      pkgs = import nixpkgs { system = "x86_64-linux";
+                              inherit overlays;
+                              inherit (haskellNix) config;
+                            };
+      flake = pkgs.helloyesod.flake {};
+
+    in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: flake // {
+        packages = flake.packages // {
+          default = flake.packages."helloyesod:exe:helloyesod";
+          helloyesod-wrapper = pkgs.helloyesod-wrapper;
+        };
+        apps = flake.apps // { default = flake.apps."helloyesod:exe:helloyesod"; };
+
+        legacyPackages = pkgs;
+      });
+
+  # --- Flake Local Nix Configuration ----------------------------
   nixConfig = {
     extra-substituters = ["https://cache.iog.io"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
